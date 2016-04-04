e7ac83c58329b083607ee98048a029a43c81e670
yunjing <mo13yu@hotmail.com>
2016-04-04 17:46:17 -0400

    change UI to ui

--- DrWaker/app/src/main/java/yunjingl/cmu/edu/drwaker/UI/MainActivity.java
+++ DrWaker/app/src/main/java/yunjingl/cmu/edu/drwaker/UI/MainActivity.java
@@ -1,4 +1,4 @@
-package yunjingl.cmu.edu.drwaker.UI;
+package yunjingl.cmu.edu.drwaker.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
--- DrWaker/app/src/main/java/yunjingl/cmu/edu/drwaker/UI/Settings.java
+++ DrWaker/app/src/main/java/yunjingl/cmu/edu/drwaker/UI/Settings.java
@@ -1,16 +1,10 @@
-package yunjingl.cmu.edu.drwaker.UI;
+package yunjingl.cmu.edu.drwaker.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
